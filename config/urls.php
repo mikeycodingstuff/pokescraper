4e2185c39629e0719f44047e2efc6a1041c2bed6
Mikey Ying <mikeyinguk@gmail.com>
2024-06-09 01:01:55 +0100

    config to store url values

---
+++ config/urls.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'gen_list' => 'https://pokeapi.co/api/v2/generation?limit=100000&offset=0',
+    'pokemon_list' => 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0',
+    'type_list' => 'https://pokeapi.co/api/v2/type?limit=100000&offset=0',
+];
